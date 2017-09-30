19b8994e3c0dc3c86bcbe120a7a05e73e7678512
Ethan66 <2064281269@qq.com>
2017-09-30 14:12:03 +0800

    删除 localStore 功能

--- src/App.js
+++ src/App.js
@@ -4,15 +4,8 @@ import TodoInput from "./TodoInput";
 import TodoItem from "./TodoItem";
 import "normalize.css";
 import './reset.css';
-import * as localStore from './localStore';
 
-import AV from 'leancloud-storage'
-var APP_ID = "JOVYRyQWYjR8CmL1jwM8XFW6-gzGzoHsz"
-  var APP_KEY = "KTLKcFliFSg33vR84vw9hXOR"
-  AV.init({
-      appId: APP_ID,
-    appKey: APP_KEY
-})
+
 
 
 class App extends Component {
@@ -20,7 +13,7 @@ class App extends Component {
     super(props)
       this.state={
         newTodo:"",
-        todoList: localStore.load('todoList') || []
+        todoList: []
     }
   }
   render() {
@@ -42,7 +35,6 @@ class App extends Component {
     );
   }
     componentDidUpdate(){
-      localStore.save('todoList', this.state.todoList)
     }
   addTodo(e){
     var value=e.target.value;
---
+++ src/leanCloud.js
@@ -0,0 +1,9 @@
+import AV from 'leancloud-storage'
+var APP_ID = "JOVYRyQWYjR8CmL1jwM8XFW6-gzGzoHsz"
+var APP_KEY = "KTLKcFliFSg33vR84vw9hXOR"
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+})
+
+export default AV
