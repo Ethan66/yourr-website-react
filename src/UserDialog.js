0d4e512ecb52f64cac8152486d92f7ce50c2a27e
Ethan66 <2064281269@qq.com>
2017-09-30 14:43:35 +0800

    切换登录和注册

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './UserDialog.css'
   export default class UserDialog extends Component{
+  constructor(props){
+    super(props)
+    this.state={
+      selected: "signUp"
+    }
+  }
   render(){
         return (
           <div className="UserDialog-Wrapper">
               <div className="UserDialog">
-                <nav>
-                  <input type="radio"/> 注册
-                  <input type="radio"/> 登录
+                <nav onChange={this.switch.bind(this)}>
+                  <input type="radio" value="signUp" checked={this.state.selected==="signUp"}/> 注册
+                  <input type="radio" value="signIn" checked={this.state.selected==="signIn"}/> 登录
                 </nav>
                 <div className="panes">
                 <form className="signUp"> {/* 注册*/}
@@ -40,5 +46,10 @@ import './UserDialog.css'
               </div>
             </div>
           )
+      }
+      switch(e){
+        this.setState({
+          selected: e.target.value
+        })
       }
 }
