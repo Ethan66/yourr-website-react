131f9ec079150635a90fa2a69007e91ec124e5e8
Ethan66 <2064281269@qq.com>
2017-09-30 10:33:23 +0800

    测试 leancloud 功能

--- src/App.js
+++ src/App.js
@@ -14,6 +14,14 @@ var APP_ID = "JOVYRyQWYjR8CmL1jwM8XFW6-gzGzoHsz"
     appKey: APP_KEY
 })
 
+var TestObject = AV.Object.extend('TestObject')
+  var testObject = new TestObject()
+  testObject.save({
+      words: 'Hello World!'
+}).then(function(object) {
+    alert('LeanCloud Rocks!')
+  })
+
 class App extends Component {
   constructor(props){
     super(props)
