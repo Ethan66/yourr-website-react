9114ebe212cb0147f49a862416145b7e8c81dc06
Ethan66 <425734130@qq.com>
2017-10-08 22:54:44 +0800

    modify

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -1,82 +1,68 @@
 import React, { Component } from 'react';
 import './UserDialog.css'
-import {signUp,signIn} from './leanCloud'
+import {signUp,signIn, sendPasswordResetEmail   } from './leanCloud'
+import deepCopy from "./deepCopy";
+import ForgotPasswordForm from './ForgotPasswordForm'
+import SignInOrSignUp from './SignInOrSignUp'
   export default class UserDialog extends Component{
   constructor(props){
     super(props)
     this.state={
-      selected: "signUp",
+      selectedTab: 'signInOrSignUp', // 'forgotPassword'
       formData: {
+        email: '',
         username: "",
         password: ""
       }
     }
   }
   render(){
-    let signUpForm = (
-          <form className="signUp" onSubmit={this.signUp.bind(this)}> {/* 注册*/}
-              <div className="row">
-                <label>用户名</label>
-                <input type="text" value={this.state.formData.username} onChange={this.changeFormData.bind(this,'username')}/>
-              </div>
-              <div className="row">
-                <label>密码</label>
-                <input type="password" value={this.state.formData.password} onChange={this.changeFormData.bind(this,'password')}/>
-              </div>
-              <div className="row actions">
-                <button type="submit">注册</button>
-              </div>
-            </form>
-          )
-        let signInForm = (
-          <form className="signIn" onSubmit={this.signIn.bind(this)}> {/* 登录*/}
-              <div className="row">
-                <label>用户名</label>
-                <input type="text" value={this.state.formData.username} onChange={this.changeFormData.bind(this,'username')}/>
-              </div>
-              <div className="row">
-                <label>密码</label>
-                <input type="password" value={this.state.formData.password} onChange={this.changeFormData.bind(this,'password')}/>
-             </div>
-              <div className="row actions">
-                <button type="submit">登录</button>
-              </div>
-            </form>
-          )
     return (
           <div className="UserDialog-Wrapper">
               <div className="UserDialog">
-                <nav onChange={this.switch.bind(this)}>
-                  <label>
-                      <input type="radio" value="signUp" checked={this.state.selected==="signUp"} onChange={this.switch.bind(this)}/>
-                      注册
-                  </label>
-                  <label>
-                      <input type="radio" value="signIn" checked={this.state.selected==="signIn"} onChange={this.switch.bind(this)}/>
-                      登录
-                  </label>
-                </nav>
-                <div className="panes">
-                  {this.state.selected === 'signUp' ? signUpForm : null}
-                  {this.state.selected === 'signIn' ? signInForm : null}
-                </div>
+                  {
+                      this.state.selectedTab === 'signInOrSignUp' ?
+                          <SignInOrSignUp
+                              formData={this.state.formData}
+                              onSignIn={this.signIn.bind(this)}
+                              onSignUp={this.signUp.bind(this)}
+                              onChange={this.changeFormData.bind(this)}
+                              onForgotPassword={this.showForgotPassword.bind(this)}
+                          /> :
+                      <ForgotPasswordForm
+                          formData={this.state.formData}
+                          onSubmit={this.resetPassword.bind(this)}
+                          onChange={this.changeFormData.bind(this)}
+                          onSignIn={this.returnToSignIn.bind(this)}
+                      />
+                  }
               </div>
             </div>
           )
       }
-      switch(e){
-        this.setState({
-          selected: e.target.value
-        })
+      showForgotPassword(){
+          let stateCopy = JSON.parse(JSON.stringify(this.state))
+          stateCopy.selectedTab = 'forgotPassword'
+          this.setState(stateCopy)
+      }
+      returnToSignIn(){
+          let stateCopy = JSON.parse(JSON.stringify(this.state))
+          stateCopy.selectedTab = 'signInOrSignUp'
+          this.setState(stateCopy)
+      }
+      resetPassword(e){
+          e.preventDefault()
+          sendPasswordResetEmail(this.state.formData.email)
       }
     changeFormData(key,e){
-      let stateCopy = JSON.parse(JSON.stringify(this.state));
+          let stateCopy=deepCopy(this.state);
+      // let stateCopy = JSON.parse(JSON.stringify(this.state));
         stateCopy.formData[key]=e.target.value;
         this.setState(stateCopy);
     }
     signUp(e){
         e.preventDefault()
-            let {username, password} = this.state.formData
+            let {email,username, password} = this.state.formData
                 let success = (user)=>{
                   this.props.onSignUp.call(null, user)
                 }
@@ -85,12 +71,24 @@ import {signUp,signIn} from './leanCloud'
                     case 202:
                       alert('用户名已被占用')
                       break
+                    case 201:
+                        alert('密码不能为空')
+                        break
+                    case 200:
+                        alert('用户名不能为空')
+                        break
+                    case 217:
+                        alert('无效的用户名，不允许空白用户名')
+                        break
+                    case 218:
+                        alert('无效的密码，不允许空白密码')
+                        break
                     default:
                       alert(error)
                       break
                   }
                 }
-            signUp(username, password, success, error)
+            signUp(email, username, password, success, error)
     }
     signIn(e){
         e.preventDefault()
@@ -103,6 +101,16 @@ import {signUp,signIn} from './leanCloud'
                     case 210:
                       alert('用户名与密码不匹配')
                       break
+                    case 201:
+                        alert('密码不能为空')
+                        break
+                    case 200:
+                        alert('用户名不能为空')
+                        break
+                    case 211:
+                        alert('用户名不存在')
+                        break
+
                     default:
                       alert(error)
                       break
